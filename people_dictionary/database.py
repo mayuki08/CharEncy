069a80bad854b2a9ef10dcc92c04c063f137cd3d
camper0605 <155131770+camper0605@users.noreply.github.com>
2025-08-28 21:08:39 +0900

    database.pyの更新

--- people_dictionary/database.py
+++ people_dictionary/database.py
@@ -8,6 +8,7 @@ def init_db():
         CREATE TABLE IF NOT EXISTS people (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
+            furigana TEXT,
             job TEXT,
             met_date TEXT,
             memo TEXT
@@ -16,18 +17,18 @@ def init_db():
     conn.commit()
     conn.close()
 
-def add_person(name, job, met_date, memo):
+def add_person(name, furigana, job, met_date, memo):
     conn = sqlite3.connect('people.db')
     c = conn.cursor()
-    c.execute('INSERT INTO people (name, job, met_date, memo) VALUES (?, ?, ?, ?)', 
-              (name, job, met_date, memo))
+    c.execute('INSERT INTO people (name, furigana, job, met_date, memo) VALUES (?, ?, ?, ?, ?)',
+              (name, furigana, job, met_date, memo))
     conn.commit()
     conn.close()
 
 def get_all_people():
     conn = sqlite3.connect('people.db')
     c = conn.cursor()
-    c.execute('SELECT id, name, job, met_date FROM people')
+    c.execute('SELECT id, name, furigana, job, met_date FROM people')
     rows = c.fetchall()
     conn.close()
     return rows
@@ -44,17 +45,17 @@ def search_people(keyword):
 def get_person_by_id(person_id):
     conn = sqlite3.connect('people.db')
     c = conn.cursor()
-    c.execute("SELECT id, name, job, met_date, memo FROM people WHERE id = ?", (person_id,))
+    c.execute("SELECT id, name, furigana, job, met_date, memo FROM people WHERE id = ?", (person_id,))
     person = c.fetchone()
     conn.close()
     return person
 
-def update_person(person_id, name, job, met_date, memo):
+def update_person(person_id, name, furigana, job, met_date, memo):
     conn = sqlite3.connect('people.db')
     c = conn.cursor()
     c.execute("""
-        UPDATE people SET name=?, job=?, met_date=?, memo=? WHERE id=?
-    """, (name, job, met_date, memo, person_id))
+        UPDATE people SET name=?, furigana=?, job=?, met_date=?, memo=? WHERE id=?
+    """, (name, furigana, job, met_date, memo, person_id))
     conn.commit()
     conn.close()
 
